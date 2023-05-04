142e948ac217f7751cc54a17633c5e8ea8ae391d
Winston Chiong <winstonchiong@berkeley.edu>
2023-05-03 18:05:06 -0700

    added homepage cards

--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,33 @@
-import dash 
-from dash import html, dcc 
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path="/")
 
 layout = \
     html.Div([
-        html.H1("THIS IS THE HOME PAGE!"),
-        html.Img(src=r'assets/cat1.jpg', alt='image', className="Mac")
+        dbc.Card([
+            dbc.CardHeader([
+                html.H5("Energy Delivered Today", className="card-title"),
+            ]),
+            dbc.CardBody([
+                html.H2("420 kWh")
+            ])
+        ]),
+        dbc.Card([
+            dbc.CardHeader([
+                html.H5("Users Today", className="card-title"),
+            ]),
+            dbc.CardBody([
+                html.H2("420"),
+            ])
+        ]),
+        dbc.Card([
+            dbc.CardHeader([
+                html.H5("Peak Power This Month", className="card-title"),
+            ]),
+            dbc.CardBody([
+                html.H2("420 W"),
+            ])
+        ])
     ])
