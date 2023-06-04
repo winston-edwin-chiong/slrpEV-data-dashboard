26763130981b82635455d27dfc5ac1d36131abee
Winston Chiong <winstonchiong@berkeley.edu>
2023-06-04 00:31:18 -0700

    edited about page

--- pages/about.py
+++ pages/about.py
@@ -1,12 +1,16 @@
 import dash 
+import os
+from dash.dependencies import ClientsideFunction, Input, Output
 from dash import html, dcc
+from dotenv import load_dotenv
 
 dash.register_page(__name__, "/about")
+
+load_dotenv()
 
 layout = \
     html.Div([
         html.Div([
             html.P("The intent of this dashboard is to visualize and analyze slrpEV demand."),
-        ], className="text-center m-2")
+        ], className="text-center m-2"),
     ])
-
